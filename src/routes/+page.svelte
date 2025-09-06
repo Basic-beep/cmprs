3c4dd4c43c5571f043e5689ab08c51c47bdc8178
Basic-beep <elliottroseemail@gmail.com>
2025-09-05 22:42:02 -0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<h1>cmprs</h1>
+<p>A W.I.P Ysws</p><a href="hackclub.com">hackclub</a>
+
+<p>join the channel!<a href="https://hackclub.slack.com/archives/C09E8TSMW9F/p1757126098829419">#cmprs</a></p>
