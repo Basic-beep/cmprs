74d28e6730ced48bb48a60c8a52a4d90fca222f1
Basic-beep <elliottroseemail@gmail.com>
2025-09-13 15:30:41 -0400

    centered and added an image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
-<h1>cmprs</h1>
-<p>A W.I.P Ysws</p><a href="hackclub.com">hackclub</a>
+<img-div>
+    <img src="/cmprs-logo-1.svg" alt="A rainbow colored text box saying 'cmprs'"> 
+</img-div>
+<p>A W.I.P Ysws for <a href="hackclub.com">hackclub</a></p>
 
-<p>join the channel!<a href="https://hackclub.slack.com/archives/C09E8TSMW9F/p1757126098829419">#cmprs</a></p>
+<p>join the channel! <br><a href="https://hackclub.slack.com/archives/C09E8TSMW9F/p1757126098829419">#cmprs</a></p>
